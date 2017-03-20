276708af25bbce9ea30d4a8d8513dfafe90b65cd
linxb <linxb@ename.com>
2017-03-20 15:47:28 +0800

    delete

--- server/Version.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("RESOURCE_VERSION", 124);
-?>
--- server/contents/check/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace check;
-
-function c_SetUserName()
-{
-    $ret = array();
-    $ret['Name'] = GetParam("NewName");
-    return $ret;
-}
